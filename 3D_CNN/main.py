b33f6f94b93686249c5d42c81becfb21420a19d7
GQY <jidegaosuwo@163.com>
2024-08-26 17:53:17 +0800

    small changes

--- 3D_CNN/main.py
+++ 3D_CNN/main.py
@@ -1,236 +1,122 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.io as sio
-from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, cohen_kappa_score
-import spectral
+from sklearn.metrics import classification_report
+from Net import HybridSN_BN
+import os
 import torch
-import torchvision
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import copy
+from processing_library import *
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
 
-class_num = 16
-
-class HybridSN(nn.Module):  
-  def __init__(self, in_channels=1, out_channels=class_num):
-    super(HybridSN, self).__init__()
-    self.conv3d_features = nn.Sequential(
-        nn.Conv3d(in_channels,out_channels=8,kernel_size=(7,3,3)),
-        nn.ReLU(),
-        nn.Conv3d(in_channels=8,out_channels=16,kernel_size=(5,3,3)),
-        nn.ReLU(),
-        nn.Conv3d(in_channels=16,out_channels=32,kernel_size=(3,3,3)),
-        nn.ReLU()
-    )
-
-    self.conv2d_features = nn.Sequential(
-        nn.Conv2d(in_channels=32 * 18, out_channels=64, kernel_size=(3,3)),
-        nn.ReLU()
-    )
-
-    self.classifier = nn.Sequential(
-        nn.Linear(64 * 17 * 17, 256),
-        nn.ReLU(),
-        nn.Dropout(p=0.4),
-        nn.Linear(256, 128),
-        nn.ReLU(),
-        nn.Dropout(p=0.4),
-        nn.Linear(128, 16)
-    )
- 
-  def forward(self, x):
-    x = self.conv3d_features(x)
-    x = x.view(x.size()[0],x.size()[1]*x.size()[2],x.size()[3],x.size()[4])
-    x = self.conv2d_features(x)
-    x = x.view(x.size()[0],-1)
-    x = self.classifier(x)
-    return x
-  
-
-# 对高光谱数据 X 应用 PCA 变换
-def applyPCA(X, numComponents):
-    newX = np.reshape(X, (-1, X.shape[2]))
-    pca = PCA(n_components=numComponents, whiten=True)
-    newX = pca.fit_transform(newX)
-    newX = np.reshape(newX, (X.shape[0], X.shape[1], numComponents))
-    return newX
-
-# 对单个像素周围提取 patch 时，边缘像素就无法取了，因此，给这部分像素进行 padding 操作
-def padWithZeros(X, margin=2):
-    newX = np.zeros((X.shape[0] + 2 * margin, X.shape[1] + 2* margin, X.shape[2]))
-    x_offset = margin
-    y_offset = margin
-    newX[x_offset:X.shape[0] + x_offset, y_offset:X.shape[1] + y_offset, :] = X
-    return newX
-
-# 在每个像素周围提取 patch ，然后创建成符合 keras 处理的格式
-def createImageCubes(X, y, windowSize=5, removeZeroLabels = True):
-    # 给 X 做 padding
-    margin = int((windowSize - 1) / 2)
-    zeroPaddedX = padWithZeros(X, margin=margin)
-    # split patches
-    patchesData = np.zeros((X.shape[0] * X.shape[1], windowSize, windowSize, X.shape[2]))
-    patchesLabels = np.zeros((X.shape[0] * X.shape[1]))
-    patchIndex = 0
-    for r in range(margin, zeroPaddedX.shape[0] - margin):
-        for c in range(margin, zeroPaddedX.shape[1] - margin):
-            patch = zeroPaddedX[r - margin:r + margin + 1, c - margin:c + margin + 1]   
-            patchesData[patchIndex, :, :, :] = patch
-            patchesLabels[patchIndex] = y[r-margin, c-margin]
-            patchIndex = patchIndex + 1
-    if removeZeroLabels:
-        patchesData = patchesData[patchesLabels>0,:,:,:]
-        patchesLabels = patchesLabels[patchesLabels>0]
-        patchesLabels -= 1
-    return patchesData, patchesLabels
-
-def splitTrainTestSet(X, y, testRatio, randomState=345):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testRatio, random_state=randomState, stratify=y)
-    return X_train, X_test, y_train, y_test
-
-
+pca_components = 30
+PCA = True
+patch_size = 25
+test_ratio = 0.90
+random_state = 345
 X = sio.loadmat('E:\\Project\\Classification_Network\\3D_CNN\\data\\Indian_pines_corrected.mat')['indian_pines_corrected']
 y = sio.loadmat('E:\\Project\\Classification_Network\\3D_CNN\\data\\Indian_pines_gt.mat')['indian_pines_gt']
 
-# 用于测试样本的比例
-test_ratio = 0.90
-# 每个像素周围提取 patch 的尺寸
-patch_size = 25
-# 使用 PCA 降维，得到主成分的数量
-pca_components = 30
-
-print('Hyperspectral data shape: ', X.shape)
-print('Label shape: ', y.shape)
-
-print('\n... ... PCA tranformation ... ...')
-X_pca = applyPCA(X, numComponents=pca_components)
-print('Data shape after PCA: ', X_pca.shape)
-
-print('\n... ... create data cubes ... ...')
+# 保存包含背景值的y，以便画出整体预测图
+y_full = y
+X_pca = applyPCA(X, numComponents=pca_components, is_PCA=PCA)
 X_pca, y = createImageCubes(X_pca, y, windowSize=patch_size)
-print('Data cube X shape: ', X_pca.shape)
-print('Data cube y shape: ', y.shape)
+Xtrain, Xtest, ytrain, ytest, data_test = splitTrainTestSet(X_pca, y, test_ratio, random_state, patch_size, pca_components,is_PCA=PCA)
 
-print('\n... ... create train & test data ... ...')
-Xtrain, Xtest, ytrain, ytest = splitTrainTestSet(X_pca, y, test_ratio)
-print('Xtrain shape: ', Xtrain.shape)
-print('Xtest  shape: ', Xtest.shape)
+trainset = TrainDS(Xtrain, ytrain)
+testset = TestDS(Xtest, ytest)
+full_testset = FullTestDS(data_test, y)
 
-# 改变 Xtrain, Ytrain 的形状，以符合 keras 的要求
-Xtrain = Xtrain.reshape(-1, patch_size, patch_size, pca_components, 1)
-Xtest  = Xtest.reshape(-1, patch_size, patch_size, pca_components, 1)
-print('before transpose: Xtrain shape: ', Xtrain.shape) 
-print('before transpose: Xtest  shape: ', Xtest.shape) 
-
-# 为了适应 pytorch 结构，数据要做 transpose
-Xtrain = Xtrain.transpose(0, 4, 3, 1, 2)
-Xtest  = Xtest.transpose(0, 4, 3, 1, 2)
-print('after transpose: Xtrain shape: ', Xtrain.shape) 
-print('after transpose: Xtest  shape: ', Xtest.shape) 
-
-""" Training dataset"""
-class TrainDS(torch.utils.data.Dataset): 
-    def __init__(self):
-        self.len = Xtrain.shape[0]
-        self.x_data = torch.FloatTensor(Xtrain)
-        self.y_data = torch.LongTensor(ytrain)        
-    def __getitem__(self, index):
-        # 根据索引返回数据和对应的标签
-        return self.x_data[index], self.y_data[index]
-    def __len__(self): 
-        # 返回文件数据的数目
-        return self.len
-
-""" Testing dataset"""
-class TestDS(torch.utils.data.Dataset): 
-    def __init__(self):
-        self.len = Xtest.shape[0]
-        self.x_data = torch.FloatTensor(Xtest)
-        self.y_data = torch.LongTensor(ytest)
-    def __getitem__(self, index):
-        # 根据索引返回数据和对应的标签
-        return self.x_data[index], self.y_data[index]
-    def __len__(self): 
-        # 返回文件数据的数目
-        return self.len
-
-# 创建 trainloader 和 testloader
-trainset = TrainDS()
-testset  = TestDS()
+# 创建 DataLoader
 train_loader = torch.utils.data.DataLoader(dataset=trainset, batch_size=128, shuffle=True)
-test_loader  = torch.utils.data.DataLoader(dataset=testset,  batch_size=128, shuffle=False)
+test_loader = torch.utils.data.DataLoader(dataset=testset, batch_size=128, shuffle=False)
+full_test_loader = torch.utils.data.DataLoader(dataset=full_testset, batch_size=128, shuffle=False)
 
-def train(net):
+def train(net, is_train, model_path):
+    current_loss_his = []
+    current_Acc_his = []
 
-  current_loss_his = []
-  current_Acc_his = []
+    model_path = model_path 
 
-  best_net_wts = copy.deepcopy(net.state_dict())
-  best_acc = 0.0
-
-  criterion = nn.CrossEntropyLoss()
-  optimizer = optim.Adam(net.parameters(), lr=0.001)
-
-  # 开始训练
-  total_loss = 0
-  for epoch in range(100):
-      net.train()  # 将模型设置为训练模式
-      for i, (inputs, labels) in enumerate(train_loader):
-          inputs = inputs.to(device)
-          labels = labels.to(device)
-          # 优化器梯度归零
-          optimizer.zero_grad()
-          # 正向传播 +　反向传播 + 优化 
-          outputs = net(inputs)
-          loss = criterion(outputs, labels)
-          loss.backward()
-          optimizer.step()
-          total_loss += loss.item()
-
-      # 每个epoch结束后，在验证集上测试一下当前模型的准确率
-      net.eval()   # 将模型设置为验证模式
-      current_acc = test_acc(net)
-      current_Acc_his.append(current_acc)
-
-      # 如果当前准确率大于最好的准确率，就更新最好的准确率和对应的网络参数
-      if current_acc > best_acc:
-        best_acc = current_acc
+    if is_train:
         best_net_wts = copy.deepcopy(net.state_dict())
-        torch.save(net.best_net_wts(), 'best_model.pth')
+        best_acc = 0.0
 
-      print('[Epoch: %d]   [loss avg: %.4f]   [current loss: %.4f]  [current acc: %.4f]' %(epoch + 1, total_loss/(epoch+1), loss.item(), current_acc))
-      current_loss_his.append(loss.item())
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-  print('Finished Training')
-  print("Best Acc:%.4f" %(best_acc))
+        # 开始训练
+        total_loss = 0
+        for epoch in range(100):
+            net.train()  # 将模型设置为训练模式
+            for i, (inputs, labels) in enumerate(train_loader):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                # 优化器梯度归零
+                optimizer.zero_grad()
+                # 正向传播 + 反向传播 + 优化 
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.item()
 
-  # load best model weights
-  net.load_state_dict(best_net_wts)
+            # 每个epoch结束后，在验证集上测试一下当前模型的准确率
+            net.eval()   # 将模型设置为验证模式
+            current_acc = test_acc(net)
+            current_Acc_his.append(current_acc)
 
-  return net,current_loss_his,current_Acc_his
+            # 如果当前准确率大于最好的准确率，就更新最好的准确率和对应的网络参数
+            if current_acc > best_acc:
+                best_acc = current_acc
+                best_net_wts = copy.deepcopy(net.state_dict())
+                torch.save(best_net_wts, model_path)
 
+            print('[Epoch: %d]   [loss avg: %.4f]   [current loss: %.4f]  [current acc: %.4f]' 
+                  % (epoch + 1, total_loss / (epoch + 1), loss.item(), current_acc))
+            current_loss_his.append(loss.item())
+
+        print('Finished Training')
+        print("Best Acc:%.4f" % (best_acc))
+
+        # load best model weights
+        net.load_state_dict(best_net_wts)
+
+    else:
+        # 如果不进行训练，检查是否存在预训练权重文件
+        if os.path.exists(model_path):
+            print(f"Loading pre-trained weights from {model_path}")
+            net.load_state_dict(torch.load(model_path))
+            net.eval()   # 将模型设置为验证模式
+            current_acc = test_acc(net)
+            current_Acc_his.append(current_acc)
+        else:
+            raise FileNotFoundError(f"No pre-trained weights found at {model_path}. Please train the model first.")
+
+    return net, current_loss_his, current_Acc_his
 def test_acc(net):
-  count = 0
-  # 模型测试
-  for inputs, _ in test_loader:
-      inputs = inputs.to(device)
-      outputs = net(inputs)
-      outputs = np.argmax(outputs.detach().cpu().numpy(), axis=1)
-      if count == 0:
-          y_pred_test =  outputs
-          count = 1
-      else:
-          y_pred_test = np.concatenate( (y_pred_test, outputs) )
+  
+    count = 0
+    # 模型测试
+    for inputs, _ in test_loader:
+        inputs = inputs.to(device)
+        outputs = net(inputs)
+        outputs = np.argmax(outputs.detach().cpu().numpy(), axis=1)
+        if count == 0:
+            y_pred_test =  outputs
+            count = 1
+        else:
+            y_pred_test = np.concatenate( (y_pred_test, outputs) )
 
-  # 生成分类报告
-  classification = classification_report(ytest, y_pred_test, digits=4)
-  index_acc = classification.find('weighted avg')
-  accuracy = classification[index_acc+17:index_acc+23]
-  # 加载训练好的模型，在全数据集上进行测试，并画出预测图
-  return float(accuracy)
+    # 生成分类报告
+    classification = classification_report(ytest, y_pred_test, digits=4)
+    index_acc = classification.find('weighted avg')
+    accuracy = classification[index_acc+17:index_acc+23]
+    print("Accuracy without background: %.4f" % (float(accuracy)))
+    return float(accuracy)
+
 
 # 测试GPU是否可用
 if torch.cuda.is_available():
@@ -242,9 +128,64 @@ else:
 
 
 # 网络放到对应的设备上
-net = HybridSN().to(device)
-# 训练
-net,current_loss_his,current_Acc_his = train(net)
+net = HybridSN_BN().to(device)
 
-test_acc = test_acc(net)
-print("Test Acc:%.4f" %(test_acc))
+# 训练
+model_save_path = 'E:/Project/Classification_Network/3D_CNN/best_model.pth'
+
+# 训练网络
+net, current_loss_his, current_Acc_his = train(net, is_train=False, model_path=model_save_path)
+
+# 全数据集测试
+
+index = 0
+# 全数据测试
+for inputs, _ in full_test_loader:
+    inputs = inputs.to(device)
+    outputs = net(inputs)
+    outputs = np.argmax(outputs.detach().cpu().numpy(), axis=1)
+    if index == 0:
+        y_pred_full =  outputs
+        index = 1
+    else:
+        y_pred_full = np.concatenate( (y_pred_full, outputs) )
+
+y_pred_full = y_pred_full +1
+y_full = y_full.flatten()
+y_pred_full_all = copy.deepcopy(y_full)
+count = 0
+for j in range(len(y_pred_full_all)):
+    if y_pred_full_all[j] != 0:
+        y_pred_full_all[j] = y_pred_full[count]
+        count = count + 1
+
+cmap = mcolors.ListedColormap([
+    'blue',      # 类别0，蓝色
+    'red',       # 类别1
+    'green',     # 类别2
+    'yellow',    # 类别3
+    'purple',    # 类别4
+    'orange',    # 类别5
+    'pink',      # 类别6
+    'brown',     # 类别7
+    'gray',      # 类别8
+    'cyan',      # 类别9
+    'magenta',   # 类别10
+    'lime',      # 类别11
+    'darkred',   # 类别12
+    'lightblue', # 类别13
+    'lightgreen',# 类别14
+    'darkorange',# 类别15
+    'gold'       # 类别16
+])
+
+bounds = np.arange(18) - 0.5  # 17个类别，需要18个边界值
+norm = mcolors.BoundaryNorm(bounds, cmap.N)
+
+y_pred_full_all = y_pred_full_all.reshape((145, 145))
+# 绘制 your_array 图像
+plt.figure(figsize=(8, 8))
+plt.imshow(y_pred_full_all, cmap=cmap, norm=norm)
+plt.colorbar(ticks=np.arange(17))  # 添加颜色条，显示类别编号
+plt.title("Classification Result")
+plt.show()
